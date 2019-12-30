b9f9dd7e3c560746ee09e9174e3d6ff2ff9f5065
c-tsalidis <ctsalidis1999@gmail.com>
2019-12-30 18:31:56 +0100

    Added dynamic rows of circles creation

---
+++ Assets/Scripts/GradientColor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GradientColor : MonoBehaviour {
+    private float i = 0; // counter to control the lerp
+    [SerializeField] private Color startingColor;
+    [SerializeField] private Color endingColor;
+    [SerializeField] private float rate = 10; // number of times per second the new color is chosen 
+    [SerializeField] private GameObject ground;
+    private Material groundMaterial;
+    [SerializeField] private Camera _camera;
+    private Color newColor;
+
+    void Start() {
+        if(_camera == null) _camera = Camera.main;
+        startingColor = new Color(Random.value, Random.value, Random.value);
+        endingColor = new Color(Random.value, Random.value, Random.value);
+        groundMaterial = ground.GetComponent<Renderer>().material;
+    }
+
+    void Update() {
+        i += Time.deltaTime * rate;
+        newColor = Color.Lerp(startingColor, endingColor, i);
+        _camera.backgroundColor = newColor;
+        groundMaterial.color = newColor;
+        if (i >= 1) {
+            i = 0;
+            startingColor = new Color(Random.value, Random.value, Random.value);
+            endingColor = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+}
--- Assets/Scripts/ProcessAudioData.cs
+++ Assets/Scripts/ProcessAudioData.cs
@@ -1,40 +1,79 @@
 ﻿using UnityEngine;
-using Random = UnityEngine.Random;
+using UnityEngine.Experimental.PlayerLoop;
 
 [RequireComponent(typeof(AudioSource))]
 public class ProcessAudioData : MonoBehaviour {
     private Camera camera;
-    private int spectrumLength = 256 * 2;
-    private float [] spectrum;
+    [SerializeField] private int numRows = 10;
+    private Row [] rows;
+    private int spectrumLength = 256;
     [SerializeField] private GameObject cube;
     private GameObject[] cubes;
     [SerializeField] private int scaleOffset = 1000;
-    private float distanceOffset = 100;
-    [SerializeField] private int scale = 10;
+    private float distanceOffset;
+    [SerializeField] private int scale;
+    [SerializeField] private int rotationSpeed = 10;
+    private GradientColor gradientColor;
     private void Awake() {
         camera = Camera.main;
-        spectrum = new float[spectrumLength];
-        cubes = new GameObject[spectrumLength];
-        float degrees = spectrumLength / 360;
-        distanceOffset = cube.transform.localScale.magnitude * 50;
-        for (int i = 0; i < spectrum.Length; i++) {
-            GameObject go = Instantiate(cube, transform, true);
-            go.name = "Cube_" + i;
-            transform.eulerAngles = new Vector3(0, degrees * i, 0);
-            go.transform.position = Vector3.forward * distanceOffset;
-            go.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-            cubes[i] = go;
+        rows = new Row[numRows];
+        gradientColor = GetComponent<GradientColor>();
+        distanceOffset = cube.transform.localScale.x;
+        // camera.transform.rotation = Quaternion.identity;
+        // scale = (int) (spectrumLength / distanceOffset);
+        for (int i = 0; i < numRows; i++) {
+            GameObject level = new GameObject();
+            level.transform.SetParent(transform);
+            level.name = "Level_" + i;
+            transform.rotation = Quaternion.identity;
+            transform.position = new Vector3(0, 0, - i * 2);
+            rows[i] = new Row(transform, level.transform, spectrumLength, cube, i + 1, Random.ColorHSV());
         }
     }
 
+
     private void Update() {
-        camera.transform.Rotate(0, 0.25f, 0);
+        float [] spectrum = new float[spectrumLength];
+        // camera.transform.position = Vector3.up;
+        // camera.transform.Rotate(0, 0.25f, 0);
+        // camera.transform.LookAt(Vector3.zero);
+        // camera.transform.Translate(Vector3.right * Time.deltaTime * cameraRotationSpeed);
+        transform.Rotate(0.0f, rotationSpeed * Time.deltaTime, 0.0f);
         AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
-        for (int i = 0; i < spectrum.Length; i++) {
-            if (cubes[i] != null) {
-                cubes[i].transform.localScale = new Vector3(cubes[i].transform.localScale.x, spectrum[i] * scaleOffset, cubes[i].transform.localScale.z);
-                // cubes[i].transform.localScale = new Vector3(scale, spectrum[i] * scaleOffset, scale);
+        foreach (var row in rows) row.Update(spectrum, scaleOffset);
+    }
+    
+    private class Row {
+        private int rowNumber;
+        private GameObject [] cubes;
+        public Row(Transform transform, Transform levelTransform, int spectLength, GameObject cube, int _rowNumber, Color c) {
+            this.cubes = new GameObject[spectLength];
+            // float degrees = spectrumLength < 360 ? 360.0f / spectrumLength : spectrumLength / 360.0f;
+            float degrees = 360.0f / spectLength;
+            for (int i = 0; i < spectLength; i++) {
+                GameObject go = Instantiate(cube, levelTransform, true);
+                go.name = "Cube_" + i;
+                transform.eulerAngles = new Vector3(0, i * degrees, 0);
+                go.transform.position = Vector3.forward * 50;
+                // if(c == null) 
+                    c = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                go.GetComponent<Renderer>().material.color = c;
+                go.GetComponent<Renderer>().material.SetColor("_EmissionColor", c);
+                cubes[i] = go;
+            }
+            this.rowNumber = _rowNumber;
+        }
+
+        public void Update(float [] spectrum, int scaleOffset) {
+            for (int i = 0; i < spectrum.Length; i++) {
+                if (this.cubes[i] != null) {
+                    // Debug.Log(spectrum[i]);
+                    this.cubes[i].transform.localScale = new Vector3(cubes[i].transform.localScale.x, spectrum[i] * scaleOffset * this.rowNumber,
+                        cubes[i].transform.localScale.z);
+                    // cubes[i].transform.localScale = new Vector3(distanceOffset, spectrum[i] * scaleOffset, distanceOffset);
+                }
             }
         }
     }
 }
+
